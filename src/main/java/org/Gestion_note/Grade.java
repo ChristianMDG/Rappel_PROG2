72eb06ccd19e3da22f7a4389cae6b4b89270069b
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-11-16 22:55:22 +0300

    changer username

--- src/main/java/org/Gestion_note/Grade.java
+++ src/main/java/org/Gestion_note/Grade.java
@@ -4,8 +4,19 @@ import java.time.Instant;
 import java.util.List;
 
 public class Grade {
+    private Exam exam;
+    private Student student;
     private double initialValue;
     private List<GradeHistoric> gradeHistoric;
-    private Instant datetime;
 
+    public Grade(Exam exam, Student student, double initialValue, List<GradeHistoric> gradeHistoric) {
+        this.exam = exam;
+        this.student = student;
+        this.initialValue = initialValue;
+        this.gradeHistoric = gradeHistoric;
+    }
+
+    public void addGradeChange(double new){
+
+    }
 }
--- src/main/java/org/Gestion_note/GradeHistoric.java
+++ src/main/java/org/Gestion_note/GradeHistoric.java
@@ -1,7 +1,33 @@
 package org.Gestion_note;
 
+import java.time.Instant;
+
 public class GradeHistoric {
     private double oldValue;
     private double newValue;
-    private
+    private Instant changeDate;
+    private String motif;
+
+    public GradeHistoric(double oldValue, double newValue, Instant changeDate, String motif) {
+        this.oldValue = oldValue;
+        this.newValue = newValue;
+        this.changeDate = changeDate;
+        this.motif = motif;
+    }
+
+    public double getOldValue() {
+        return oldValue;
+    }
+
+    public double getNewValue() {
+        return newValue;
+    }
+
+    public Instant getChangeDate() {
+        return changeDate;
+    }
+
+    public String getMotif() {
+        return motif;
+    }
 }
