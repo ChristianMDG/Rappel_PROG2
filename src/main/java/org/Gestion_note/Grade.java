55e8645bbcdd7c2cb1fdaf2c50d8cbfdceae97d7
christianMDG <christian.ravelojaona@student.passerellesnumeriques.org>
2025-11-17 12:00:11 +0300

    finish note

--- src/main/java/org/Gestion_note/Grade.java
+++ src/main/java/org/Gestion_note/Grade.java
@@ -1,22 +1,42 @@
 package org.Gestion_note;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Grade {
     private Exam exam;
     private Student student;
     private double initialValue;
-    private List<GradeHistoric> gradeHistoric;
+    private List<GradeHistoric> historic;
 
-    public Grade(Exam exam, Student student, double initialValue, List<GradeHistoric> gradeHistoric) {
+    public Grade(Exam exam, Student student, double value) {
         this.exam = exam;
         this.student = student;
-        this.initialValue = initialValue;
-        this.gradeHistoric = gradeHistoric;
+        this.initialValue = value;
+        this.historic = new ArrayList<>();
+        this.historic.add(new GradeHistoric(value,value,Instant.now(),"first note"));
     }
 
-    public void addGradeChange(double new){
+    public Exam getExam() {
+        return exam;
+    }
 
+    public Student getStudent() {
+        return student;
+    }
+
+    public double getInitialValue() {
+        return initialValue;
+    }
+
+    public List<GradeHistoric> getHistoric() {
+        return historic;
+    }
+
+    public void addGradeChange(double newValue , String motif){
+        double oldValue = this.initialValue;
+        this.initialValue = newValue;
+        this.historic.add(new GradeHistoric(oldValue,newValue,Instant.now(),motif));
     }
 }
